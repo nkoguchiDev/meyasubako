d3aafdf6976204d6c5bd5aae13d8c62f30d03b9b
nkoguchi <nkoguchi00dev@gmail.com>
2022-10-31 22:06:11 +0900

    initial commit

---
+++ frontend/app/src/App.js
@@ -0,0 +1,11 @@
+import { BrowserRouter } from "react-router-dom";
+
+import { Router } from "./router/Router";
+
+export const App = () => {
+    return (
+        <BrowserRouter>
+            <Router />
+        </BrowserRouter>
+    );
+};
