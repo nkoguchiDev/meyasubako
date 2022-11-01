4d5691dc5af777a77faa9897c9a234f911b23c9a
nkoguchi <nkoguchi00dev@gmail.com>
2022-11-02 00:44:41 +0900

    k8sで使えるように変更

--- setup.sh
+++ setup.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 
-echo DB_USER=root >.env
-echo DB_PASSWORD=password >>.env
-echo DB_HOST=localhost >>.env
-echo DB_PORT=27017 >>.env
-echo SECRET_KEY=$(openssl rand -hex 32) >>.env
+echo HASHED_SECRET_KEY=password >.env
 
 cp .env ./backend
 
