e19c513a7f882e0a44ec36f25a318f01a9c5aeb3
nkoguchi <nkoguchi00dev@gmail.com>
2022-11-02 00:44:33 +0900

    k8sで使えるように変更

---
+++ app_image_push.sh
@@ -0,0 +1,5 @@
+docker tag meyasubako-ui nkoguchi00dev/meyasubako-ui
+docker push nkoguchi00dev/meyasubako-ui:latest
+
+docker tag meyasubako-ui nkoguchi00dev/meyasubako-api
+docker push nkoguchi00dev/meyasubako-api:latest
