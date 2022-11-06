a72c443249b37314736d0e79c5729991501dbccb
nkoguchi <nkoguchi00dev@gmail.com>
2022-11-07 01:55:20 +0900

    subject api 追加

--- backend/app/api/api_v1/api.py
+++ backend/app/api/api_v1/api.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from app.api.api_v1.endpoints import message
+from app.api.api_v1.endpoints import subject
 
 api_router = APIRouter()
-api_router.include_router(message.router, prefix="/messages", tags=["message"])
+api_router.include_router(subject.router, prefix="/subject", tags=["subject"])
