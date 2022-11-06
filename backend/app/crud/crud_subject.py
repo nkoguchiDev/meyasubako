64c45417862fcabb2d326150d6cc44ba0223462b
nkoguchi <nkoguchi00dev@gmail.com>
2022-11-07 01:57:00 +0900

    WIP

--- backend/app/crud/crud_subject.py
+++ backend/app/crud/crud_subject.py
@@ -1,32 +1,21 @@
-from uuid import uuid4
-import datetime
-
-from app.crud.base import CRUDBase
 from app.models import Subject
-from app.schemas import SubjectCreate
+from app.utils import create_uuid, get_date_time_jst
+from app.crud.base import CRUDBase
 
 
-class CRUDSubject(CRUDBase[Subject, SubjectCreate, SubjectCreate]):
-
-    def get(self, uuid) -> Subject:
-        return Subject.objects(uuid=uuid).first()
+class CRUDSubject(CRUDBase[Subject]):
 
     def create(
             self,
-            obj_in: SubjectCreate) -> Subject:
-
-        t_delta = datetime.timedelta(hours=9)
-        JST = datetime.timezone(t_delta, 'JST')
-        now = datetime.datetime.now(JST)
-
-        uuid = uuid4().hex
-        db_obj = Subject(
+            name: str) -> Subject:
+        uuid = create_uuid()
+        subject_obj = Subject(
             uuid=uuid,
-            name=obj_in.name,
-            date=now.strftime('%Y-%m-%d %H:%M')
+            name=name,
+            date=get_date_time_jst()
         )
-        db_obj.save()
+        subject_obj.save()
         return Subject.objects(uuid=uuid).first()
 
 
-Subject = CRUDSubject(Subject)
+subject = CRUDSubject(Subject)
