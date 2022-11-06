1339f2162060a1cfdef3413b42df77aff2832a7c
nkoguchi <nkoguchi00dev@gmail.com>
2022-11-07 01:57:06 +0900

    WIP

--- backend/app/models/subject.py
+++ backend/app/models/subject.py
@@ -4,12 +4,11 @@ from mongoengine.fields import StringField
 
 class Subject(Document):
     uuid = StringField(unique=True, required=True)
-    _type = StringField(default="subject")
     name = StringField(required=True)
     date = StringField(required=True)
 
     meta = {
-        'db_alias': 'mongodb',
+        'db_alias': 'meyasubako',
         'collection': 'subject',
         'max_documents': 1000,
         'max_size': 2000000
