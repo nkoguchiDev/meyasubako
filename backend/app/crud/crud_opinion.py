97d11cab550021c5fbbe789e85ca09c80f39c661
nkoguchi <nkoguchi00dev@gmail.com>
2022-11-08 23:53:27 +0900

    opinion 取得機能の追加

--- backend/app/crud/crud_opinion.py
+++ backend/app/crud/crud_opinion.py
@@ -1,9 +1,14 @@
+from typing import List
+
 from app.models import Opinion, Subject
 from app.utils import create_uuid, get_date_time_jst
 from app.crud.base import CRUDBase
 
 
 class CRUDOpinion(CRUDBase[Opinion]):
+
+    def get_list(self, subject) -> List[Opinion]:
+        return Opinion.objects(subject=subject)
 
     def create(
             self,
