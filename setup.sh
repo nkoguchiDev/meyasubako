d831ce7d3da14f0b0bdbd51790fafff52a94435d
nkoguchi <nkoguchi00dev@gmail.com>
2022-11-09 23:04:22 +0900

    dbを使用できるように変更

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-echo HASHED_SECRET_KEY=password >.env
+echo DB_USER=root >.env
+echo DB_PASSWORD=password >>.env
+echo DB_HOST=localhost >>.env
+echo DB_PORT=27017 >>.env
 
 cp .env ./backend
 
