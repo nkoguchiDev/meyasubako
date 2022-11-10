b9385265f9feed638f1e270b9b36fa83ce7f0d5a
nkoguchi <nkoguchi00dev@gmail.com>
2022-11-10 23:00:55 +0900

    opinion api publish

--- backend/app/api/api_v1/api.py
+++ backend/app/api/api_v1/api.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
-from app.api.api_v1.endpoints import subject
+from app.api.api_v1.endpoints import subject, opinion
 
 api_router = APIRouter()
-api_router.include_router(subject.router, prefix="/subject", tags=["subject"])
+api_router.include_router(subject.router, prefix="/subjects", tags=["subject"])
+api_router.include_router(opinion.router, prefix="/opinions", tags=["opinion"])
