9d6bf80a27fb43143aa1905250cd2315998f40a3
nkoguchi <nkoguchi00dev@gmail.com>
2022-11-12 22:03:37 +0900

    お題機能に分類を追加

--- backend/app/crud/crud_subject.py
+++ backend/app/crud/crud_subject.py
@@ -7,12 +7,14 @@ class CRUDSubject(CRUDBase[Subject]):
 
     def create(
             self,
-            name: str) -> Subject:
+            name: str,
+            classification: str) -> Subject:
         uuid = create_uuid()
         subject_obj = Subject(
             uuid=uuid,
             name=name,
-            date=get_date_time_jst()
+            date=get_date_time_jst(),
+            classification=classification
         )
         subject_obj.save()
         return Subject.objects(uuid=uuid).first()
