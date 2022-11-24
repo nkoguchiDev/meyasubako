259ccbc8a9e094e27739b2629bbb47744982e4b1
nkoguchi <nkoguchi00dev@gmail.com>
2022-11-24 23:34:27 +0900

    切り戻し

--- frontend/app/src/pages/Home.jsx
+++ frontend/app/src/pages/Home.jsx
@@ -1,9 +1,8 @@
 import "./Home.css";
-import { SubjectModal, SubjectCard, TemporaryDrawer } from "../components/Elements";
+import { SubjectModal, SubjectCard } from "../components/Elements";
 export const Home = () => {
     return (
         <div>
-            <TemporaryDrawer />
             <SubjectCard />
             <div id="SubjectModal">
                 <SubjectModal />
