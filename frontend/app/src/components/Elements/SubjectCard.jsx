b298b2b871c79d642ddf0023ef67aa2acf726903
nkoguchi <nkoguchi00dev@gmail.com>
2022-11-24 23:35:15 +0900

    ドロアーの追加と適切な範囲で文字が収まるように変更

--- frontend/app/src/components/Elements/SubjectCard.jsx
+++ frontend/app/src/components/Elements/SubjectCard.jsx
@@ -3,6 +3,8 @@ import { useEffect } from "react";
 
 import styled from "styled-components";
 
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
@@ -32,16 +34,34 @@ export const SubjectCard = () => {
         };
     }, []);
 
+    const [state, setState] = React.useState(false);
+
+    const toggleDrawer = (open) => (event) => {
+        if (event.type === "keydown" && (event.key === "Tab" || event.key === "Shift")) {
+            return;
+        }
+
+        setState(open);
+    };
+
     return (
         <div>
             <ul id="events">
                 {subjectList.map((subject, i) => (
                     <CardList id={i} key={i}>
-                        <Card variant="outlined" sx={{ maxWidth: 350, margin: "auto" }}>
+                        <Card
+                            variant="outlined"
+                            onClick={toggleDrawer(true)}
+                            sx={{ maxWidth: 350, margin: "auto" }}
+                        >
                             <CardActionArea>
                                 <CardContent sx={{ display: "flex" }}>
                                     {SubjectIcon[subject.classification]}
-                                    <Typography variant="h5" component="div">
+                                    <Typography
+                                        variant="h5"
+                                        component="div"
+                                        sx={{ width: "250px", wordWrap: "break-word" }}
+                                    >
                                         {subject.name}
                                     </Typography>
                                 </CardContent>
@@ -50,6 +70,31 @@ export const SubjectCard = () => {
                     </CardList>
                 ))}
             </ul>
+            <Drawer anchor="right" open={state} onClose={toggleDrawer(false)}>
+                <Box
+                    sx={{ width: 400 }}
+                    role="presentation"
+                    onClick={toggleDrawer(false)}
+                    onKeyDown={toggleDrawer(false)}
+                >
+                    <Card
+                        variant="outlined"
+                        onClick={toggleDrawer(true)}
+                        sx={{ maxWidth: 350, margin: "auto" }}
+                    >
+                        <CardActionArea>
+                            <CardContent sx={{ display: "flex" }}>
+                                <Typography
+                                    variant="body1"
+                                    sx={{ width: "320px", wordWrap: "break-word" }}
+                                >
+                                    nfoemfioamdpwdkopa,mfoegreagregsrthrtshthregresahshberth
+                                </Typography>
+                            </CardContent>
+                        </CardActionArea>
+                    </Card>
+                </Box>
+            </Drawer>
         </div>
     );
 };
